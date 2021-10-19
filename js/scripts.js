369f52f014b539d37446811e533e1c274094025d
marisa-edgar <marisaedgar1212@gmail.com>
2021-10-19 12:01:49 -0700

    add light and dark mode buttons
    co-Authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-Authored-by: Chase Stokes <chasehstokes@gmail.com>'

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark");
+  });
+  
+  $(".light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light");
+  });
+  
+});
